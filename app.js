d1096f2d79f469b6f0f7b63d733d8020849365ce
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-11-05 12:56:31 -0800

    longitude & latitude app.js

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ window.addEventListener('load', () => {
         if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
 // console log to see if coordinates are received.
-            console.log(position)
-        })
+            // console.log(position)
+            // access position
+    long = position.coords.longitude;
+    lat = position.coords.latitude;  
+        });
     }
 
 
