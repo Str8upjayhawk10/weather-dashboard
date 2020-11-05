df685715943e7dcbd44fd9d6507efc41d8450dd8
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-11-05 12:55:28 -0800

    longitude & latitude location app.js

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
-// Declare variable for cache of city search
-$(document).ready(function() {
-    //attach data to id's of html
-var searchCity = $("#search-city");
-var searchButton = $("#search-button");
+// Get longitude & latitude location
+window.addEventListener('load', () => {
+//    defining coordinates
+    let long;
+    let lat;
+// create "if" statement
+        if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+// console log to see if coordinates are received.
+            console.log(position)
+        })
+    }
 
 
-
-
-    function getWeather(){
-    var searchCity = $("#search-city").val()
-
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&appid=09a1c197a194673136ea1b9c9a4cc663&units=imperial")
-    .then(function(response){
-        return response.json()
-    }).then(function(data){
-        console.log(data)
-        
-        //call UVIndex
-        //call 5day forcast
-        //possibly save it as a button below the search bar with localstorage (worry about last)
-    })
-}
-
-//5day forcast function
-//for(var i = 0; i < 40; i=i+8){} //* can replace 40 with length of list from 5day
-
-//UVindex Function
-
-//Localstorage stuff
-
-
-
-$("#search-button").click(getWeather)
-        
-})
+});
