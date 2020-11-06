03ded33f8381ca4622b01c8a492cf6c96c925245
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-11-06 12:17:20 -0800

    created four loop & Api keyes

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ let currentHumidity = $('humidity');
 let currentWindSpeed = $('#wind-speed');
 let currentUvIndex = $('#uv-index');
 let cityEl = [];
+// four loop to search if cities exist within storage
+function find (c) {
+    for (var i = 0; i < cityEl.length; i++) {
+        if(c.toUpperCase()=== cityEl[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+// Created API Key
+const APIKey = '09a1c197a194673136ea1b9c9a4cc663';
+// Display the current and future weather grabbing from city input text box
+
