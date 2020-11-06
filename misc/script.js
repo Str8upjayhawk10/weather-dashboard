38f02686179acc2984ca32d69f5702d8e931127a
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-11-06 11:36:07 -0800

    linked css & added  styled within css

--- misc/script.js
+++ misc/script.js
@@ -1,19 +1,19 @@
 
-function getWeather(){
-    var searchCity = $("#search-city").val()
+// function getWeather(){
+//     var searchCity = $("#search-city").val()
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&appid=09a1c197a194673136ea1b9c9a4cc663&units=imperial")
-    .then(function(response){
-        return response.json()
-    }).then(function(data){
-        console.log(data)
+//     fetch("https://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&appid=09a1c197a194673136ea1b9c9a4cc663&units=imperial")
+//     .then(function(response){
+//         return response.json()
+//     }).then(function(data){
+//         console.log(data)
         //attach data to id's of html
         
         //call UVIndex
         //call 5day forcast
         //possibly save it as a button below the search bar with localstorage (worry about last)
-    })
-}
+//     })
+// }
 
 //5day forcast function
 //for(var i = 0; i < 40; i=i+8){} //* can replace 40 with length of list from 5day
@@ -24,45 +24,45 @@ function getWeather(){
 
 
 
-$("#search-button").click(getWeather)
+// $("#search-button").click(getWeather)
 // Declare variable for cache of city search
-$(document).ready(function () {
-  //attach data to id's of html
-var searchCity = $("#search-city");
-    var searchButton = $("#search-button");
-    var clearButton = $("#clear-history");
-    var currentCity = $("#current-city");
-    var currentTemperature = $("#temperature");
-    var currentHumidty = $("#humidity");
-    var currentWSpeed = $("#wind-speed");
-    var currentUvindex = $("#uv-index");
-    var sCity = [];
-  // city search within cache 
+// $(document).ready(function () {
+//   //attach data to id's of html
+// var searchCity = $("#search-city");
+//     var searchButton = $("#search-button");
+//     var clearButton = $("#clear-history");
+//     var currentCity = $("#current-city");
+//     var currentTemperature = $("#temperature");
+//     var currentHumidty = $("#humidity");
+//     var currentWSpeed = $("#wind-speed");
+//     var currentUvindex = $("#uv-index");
+//     var sCity = [];
+//   // city search within cache 
 
 
-function getWeather(event) {
-    event.preventDefault();
+// function getWeather(event) {
+//     event.preventDefault();
     // var searchCity = $("#search-city").val();
     // if(searchCity.val().trim()!==''){
     //     city=searchCity.val().trim();
     //     currentWeather(city);
-    }
+    // }
 
-    fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=09a1c197a194673136ea1b9c9a4cc663&units=imperial"
-    )
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
+    // fetch(
+    // "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=09a1c197a194673136ea1b9c9a4cc663&units=imperial"
+    // )
+    // .then(function (response) {
+    //     return response.json();
+    // })
+    // .then(function (data) {
+    //     console.log(data)
     
 
         //call UVIndex
         //call 5day forcast
         //possibly save it as a button below the search bar with localstorage (worry about last)
-    });
-}
+//     });
+// }
 
   //5day forcast function
   //for(var i = 0; i < 40; i=i+8){} //* can replace 40 with length of list from 5day
@@ -71,6 +71,6 @@ function getWeather(event) {
 
   //Localstorage stuff
 
-    $("#search-button").click(getWeather),
+//     $("#search-button").click(getWeather),
 
-});
+// });
