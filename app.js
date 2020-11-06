88f079c76b91257b773b2000b922f3f409bdc808
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-11-06 11:53:23 -0800

    created varibles

---
+++ app.js
@@ -0,0 +1,12 @@
+// Declared a varible to cache city search html line:24
+let city = '';
+// Create a varible declaration
+let searchCity = $('#search-city');
+let searchButton = $('#search-button');
+let clearButton = $('#clear-history');
+let currentCity = $('#current-city');
+let currentTemperature = $('#temperature');
+let currentHumidity = $('humidity');
+let currentWindSpeed = $('#wind-speed');
+let currentUvIndex = $('#uv-index');
+let cityEl = [];
